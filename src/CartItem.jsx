9f00395112ddb71cbccab6c635b976e9ad0f1497
Ardhika Putra <ardhikaptr11@gmail.com>
2024-08-12 16:58:51 +0700

    Fixing bug

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeItem, updateQuantity } from "./CartSlice";
 import "./CartItem.css";
@@ -36,7 +36,7 @@ const CartItem = ({ onContinueShopping }) => {
 	};
 
 	const handleRemove = (item) => {
-		dispatch(removeItem(item));
+		dispatch(removeItem(item))
 	};
 
 	// Calculate total cost based on quantity for an item
